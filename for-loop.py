8ce552b93d31ba00bd57caa97ba9a21d8135d041
sasa <75481194+abrillasalma@users.noreply.github.com>
2025-11-17 09:14:14 +0700

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,9 @@
+print("Count to 10!")
+
+for x in range (0, 11):
+    print(x)
+
+myList = ['apple', 'banana', 'cherry', 'grape']
+
+for z in myList:
+    print(z)
---
+++ numeric-data.py
@@ -0,0 +1,15 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=3.14
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=5j
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(str(myValue) + " is of the data type " + str(type(myValue)))
